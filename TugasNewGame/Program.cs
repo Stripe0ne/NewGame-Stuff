cedc5ede487cb1de2e54c1429ac9aa590aaa307a
Muhammad Yasin Habiburrahman <abikdanebo@gmail.com>
2025-11-09 23:15:39 +0700

    Penugasan NewGame1

---
+++ TugasNewGame/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+        int n = int.Parse(Console.ReadLine());
+
+        // Daun
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                Console.Write(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                Console.Write(" ");
+            }
+            Console.WriteLine("*");
+        }
+    }
+}
